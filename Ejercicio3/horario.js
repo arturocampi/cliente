065aefee8ed704d768d0ff27c3f548f071d74421
arturocampi <arturo.herraez.tpz@gmail.com>
2021-11-06 00:42:15 +0100

    ej3

--- Ejercicio3/horario.js
+++ Ejercicio3/horario.js
@@ -1,19 +1,21 @@
 function generarActividades(array) {
     var tabla = document.getElementById('tabla1');
-    var filas = tabla.getElementByTagName('tr');
-    for (let i = 0; i <= 6; i++) {
-        for (let j = 0; j < filas.length - 1; j++) {
-            filas[j].innerHTML += '<td>' + array[j - 1] + '</td>'
+    var filas = tabla.getElementsByTagName('tr');
+    for (let i = 1; i <= 6; i++) {
+        for (let j = 1; j <= filas.length - 1; j++) {
+            filas[j].innerHTML += '<td>' + array[Math.floor(Math.random() * array.length)] + '</td>';
         }
     }
 }
 
 function generarFisioterapeutas(array2) {
     var tabla2 = document.getElementById('tabla2');
-    var filas2 = tabla2.getElementByTagName('tr');
-    for (let i = 0; i <= 5; i++) {
-        for (let j = 0; j < filas2.length - 1; j++) {
-            filas2[j].innerHTML += '<td>' + array2[j - 1] + '</td>'
+    var filas2 = tabla2.getElementsByTagName('tr');
+    let random;
+    for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= filas2.length - 1; j++) {
+            random = Math.random(array2.length);
+            filas2[j].innerHTML += "<td>" + array2[Math.floor(Math.random() * array2.length)] + "</td>";
         }
     }
 }
