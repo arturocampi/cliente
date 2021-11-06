392864aef6999e4ade1d35aeba2e2acb6dd66a4d
arturocampi <arturo.herraez.tpz@gmail.com>
2021-11-06 17:37:21 +0100

    done

--- Ejercicio2/fcm.js
+++ Ejercicio2/fcm.js
@@ -22,16 +22,16 @@ function calcular(edad) {
     var num = 220 - edad;
 
     var li1 = document.getElementById(1);
-    li1.textContent = `Zona de recuperación (60%-70%): ${num * 0.6}-${num * 0.7} `;
+    li1.textContent = `Zona de recuperación (60%-70%): ${Math.floor(num * 0.6)}-${Math.floor(num * 0.7)} `;
 
     var li2 = document.getElementById(2);
-    li2.textContent = `Zona aeróbica (70%-80%): ${num * 0.7}-${num * 0.8}`;
+    li2.textContent = `Zona aeróbica (70%-80%): ${Math.floor(num * 0.7)}-${Math.floor(num * 0.8)}`;
 
     var li3 = document.getElementById(3);
-    li3.textContent = `Zona anaeróbica (80%-90%): ${num * 0.8}-${num * 0.9}`;
+    li3.textContent = `Zona anaeróbica (80%-90%): ${Math.floor(num * 0.8)}-${Math.floor(num * 0.9)}`;
 
     var li4 = document.getElementById(4);
-    li4.textContent = `Línea roja (90%-100%): ${num * 0.9}-${num}`;
+    li4.textContent = `Línea roja (90%-100%): ${Math.floor(num * 0.9)}-${num}`;
 
     var calculo = document.getElementById("answer");
     calculo.textContent = `Tu frecuencia máxima es de ${num} pulsaciones`;
---
+++ Ejercicio5/verify.js
@@ -0,0 +1,84 @@
+function verifyPassword() {
+    // RECOGE LA CONTRASEÑA
+    var password = document.getElementById('password').value;
+    // COMPRUEBA QUE TENGA UNA LONGITUD MAYOR A 8
+    let menor = true;
+    if (password.length < 8) {
+        menor = false;
+        alert('Contraseña demasiado corta');
+    }
+    // COMPRUEBA QUE TENGA UNA LONGITUD MENOR A 16
+    let mayor = true;
+    if (password.length > 16) {
+        mayor = false;
+        alert('Contrasea demasiado larga');
+    }
+    // COMPRUEBA QUE NO ESTE VACÍA
+    let vacia = true;
+    if (password == '') {
+        vacia = false;
+        alert('La contraseña no puede estar vacía');
+    }
+    // BOOLEANO MAYÚSCULAS
+    let mayus = false;
+    // COMPRUEBA QUE TENGA UNA LETRA MAYÚSCULA
+    for (let i = 0; i < password.length; i++) {
+        if ((password.charAt(i) >= 'A' && password.charAt(i) <= 'Z')) {
+            mayus = true;
+            break;
+        }
+    }
+    if (!mayus) {
+        alert('La contraseña tiene que tener una letra mayúscula');
+    }
+
+    // BOOLEANO MINÚSCULAS
+    let minus = false;
+    // COMPRUEBA QUE TENGA UNA LETRA MINÚSCULA
+    for (let i = 0; i < password.length; i++) {
+        if ((password.charAt(i) >= 'a' && password.charAt(i) <= 'z')) {
+            minus = true;
+            break;
+        }
+    }
+    if (!minus) {
+        alert('La contraseña tiene que tener una letra minúscula');
+    }
+
+    // BOOLEANO NÚMERO
+    let num = false;
+    // COMPRUEBA QUE TENGA UN NÚMERO
+    for (let i = 0; i < password.length; i++) {
+        if ((password.charAt(i) >= '0' && password.charAt(i) <= '9')) {
+            num = true;
+            break;
+        }
+    }
+    if (!num) {
+        alert('La contraseña tiene que tener un número');
+    }
+
+    // COMPRUEBA QUE TENGA UN CARACTER ESPECIAL 
+    let especial = false;
+    for (let i = 0; i < password.length; i++) {
+        if ((password.charAt(i) == '-') || (password.charAt(i) == '_') || (password.charAt(i) == '@') || (password.charAt(i) == '#') || (password.charAt(i) == '$') || (password.charAt(i) == '%') || (password.charAt(i) == '&')) {
+            especial = true;
+            break;
+        }
+    }
+    if (!especial) {
+        alert('La contraseña tiene que tener un caracter especial');
+    }
+    check(menor, mayor, vacia, minus, mayus, num, especial);
+}
+
+function check(menor, mayor, vacia, minus, mayus, num, especial) {
+    var span = document.getElementById('correcto');
+    if (menor && mayor && vacia && minus && mayus && num && especial) {
+        span.style.color = 'lime';
+        span.innerHTML = 'Contraseña Correcta!';
+    } else {
+        span.style.color = 'red';
+        span.innerHTML = 'Formato de contraseña incorrecta!';
+    }
+}
