61530da445e9559f910b87370f5a6ed185e97f44
Arthur <arturo.herraez.tpz@gmail.com>
2021-11-17 21:20:28 +0100

    ej1

---
+++ PracticaUD3/Ejercicio1/class.js
@@ -0,0 +1,11 @@
+function Colegio(nombre, numAulas, numAlumnos) {
+    this.nombre;
+    this.numAulas;
+    this.numAlumnos;
+}
+
+function Alumno(dni, nombre, notaMedia) {
+    this.dni;
+    this.nombre;
+    this.notaMedia;
+}
